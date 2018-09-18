d492b6d843a9ae2f64a78c0a0e50e5ee5ec3fea0
sunvlink <sunvlink@sunvlink.local>
2018-09-18 09:16:10 +0800

    init

---
+++ ZXUCCTest/Classes/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  Pods-ZXUCCTest_Example
+//
+//  Created by sunvlink on 2018/9/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, strong) NSString *id;
+
+@end
---
+++ ZXUCCTest/Classes/Person.m
@@ -0,0 +1,12 @@
+//
+//  Person.m
+//  Pods-ZXUCCTest_Example
+//
+//  Created by sunvlink on 2018/9/17.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
